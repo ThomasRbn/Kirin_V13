dbfbf2bfe6fa2005a6a07977841851761f22d903
Thomas Robineau <robineau1u@etu.univ-lorraine.fr>
2022-04-07 18:42:30 +0200

    Ajout image

--- commands/ban.js
+++ commands/ban.js
@@ -7,30 +7,59 @@ module.exports = {
         .setName('kban')
         .setDescription('Select a member and ban them (but not really).')
         .addUserOption(option => option.setName('cible').setDescription('Membre à bannir').setRequired(true))
-        .addStringOption(option => option.setName('raison').setDescription('Raison du ban')),
+        .addStringOption(option => option.setName('raison').setDescription('Raison du ban'))
+        .addIntegerOption(option => option.setName('duree').setDescription('Durée du ban en jours')),
 
 
     async execute(interaction) {
         const cible = interaction.options.getUser('cible');
-        const raison = interaction.options.getString('raison');
+        let raison = interaction.options.getString('raison');
+        let duree = interaction.options.getInteger('duree');
+        const definitif = "Définitif";
 
-        if (cible.id === interaction.member.id)
-            return interaction.reply({content: "❌ - Vous ne pouvez pas vous bannir vous même"});
-
-        if (!interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS))
-            return interaction.reply({content: "❌ - Vous n'avez pas l'autorisation de bannir des membres"});
-
-        if (clientId === cible.id)
-            return interaction.reply({content: "❌ - Je ne peux pas me bannir moi même °=°"});
-
-        const embedBan = new MessageEmbed()
+        const embedSuccess = new MessageEmbed()
             .setColor("GREEN")
             .setTitle("✅ - Membre banni")
             .setAuthor({name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL()})
             .addField("Membre : ", cible.tag, true)
-            .addField("Raison : ", raison, true);
+            .setThumbnail("https://drive.google.com/file/d/1bAUdMwWZuTet4yN0FfDaw5VlT0uyOI6L/view?usp=sharing");
 
-        interaction.reply({embeds: [embedBan]})
-        //await cible.kick({reason: raison});
+
+        const embedFail = new MessageEmbed()
+            .setColor("RED")
+            .setTitle("❌ - Échec de la commande")
+            .setAuthor({name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL()});
+
+        if (duree === null){
+            embedSuccess.addField("Durée : ", definitif, true);
+        } else {
+            embedSuccess.addField("Durée : ", duree + " jours", true);
+        }
+
+        if (raison === undefined){
+            embedSuccess.addField("Raison : ","Aucune raison n'a été fournie");
+        } else {
+            embedSuccess.addField("Raison : ", raison);
+        }
+
+        if (cible.id === interaction.user.id) {
+            embedFail.setDescription("❌ - Vous ne pouvez pas vous bannir vous même")
+            return interaction.reply({embeds: embedFail});
+        }
+
+        if (!interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+            embedFail.setDescription("❌ - Vous n'avez pas l'autorisation de bannir des membres")
+            return interaction.reply({embeds: embedFail});
+
+        }
+
+        if (clientId === cible.id) {
+            embedFail.setDescription("❌ - Je ne peux pas me bannir moi même °=°")
+            return interaction.reply({embeds: embedFail});
+
+        }
+
+        interaction.reply({embeds: [embedSuccess]})
+        //await cible.ban({reason: raison, days: duree});
     },
 };
