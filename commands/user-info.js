b849422086ea6f7b7db4f7855fa3a625fbc944f1
Thomas Robineau <robineau1u@etu.univ-lorraine.fr>
2022-04-11 22:05:00 +0200

    AJout bouton

--- commands/user-info.js
+++ commands/user-info.js
@@ -1,5 +1,5 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
-const {MessageEmbed, Permissions} = require("discord.js");
+const {MessageEmbed, Permissions, MessageActionRow, MessageButton} = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,37 +9,62 @@ module.exports = {
     async execute(interaction) {
         const m = interaction.options.getMember('membre');
         const u = interaction.options.getUser('membre');
-        const embedOK = new MessageEmbed()
+        const perm = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('primary')
+                    .setLabel('Permissions')
+                    .setStyle('PRIMARY'),
+            );
+        const retour = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('perm')
+                    .setLabel('Retour')
+                    .setStyle('PRIMARY'),
+            );
+
+        const embedBase = new MessageEmbed()
             .setAuthor({name: "Informations sur " + u.tag})
             .setColor("PURPLE")
             .setThumbnail(u.displayAvatarURL());
-
         if (m.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-            embedOK.setDescription("__**Est administrateur du serveur**__")
+            embedBase.setDescription("__**Est administrateur du serveur**__")
         }
-
-        embedOK.addField("ID : ", m.id)
+        embedBase.addField("ID : ", m.id)
             .addField("A rejoint le :", m.joinedAt.toLocaleDateString('fr-FR'), true)
             .addField("Crée le :", u.createdAt.toLocaleDateString('fr-FR'), true)
-            .addField('\u200B', '\u200B', true)
+            .addField('\u200B', '\u200B', true);
 
+        const embedPerm = new MessageEmbed()
+            .setAuthor({name: "Permissions globales de " + u.tag})
+            .setColor("PURPLE")
 
         if (m.permissions.has(Permissions.FLAGS.KICK_MEMBERS))
-            embedOK.addField("Peut exclure :", "Oui", true);
+            embedPerm.addField("Peut exclure :", "Oui", true);
         else
-            embedOK.addField("Peut exclure : ", "Non", true);
+            embedPerm.addField("Peut exclure : ", "Non", true);
 
         if (m.permissions.has(Permissions.FLAGS.BAN_MEMBERS))
-            embedOK.addField("Peut bannir :", "Oui", true);
+            embedPerm.addField("Peut bannir :", "Oui", true);
         else
-            embedOK.addField("Peut bannir : ", "Non", true);
+            embedPerm.addField("Peut bannir : ", "Non", true);
 
         if (m.permissions.has(Permissions.FLAGS.MUTE_MEMBERS))
-            embedOK.addField("Peut rendre muet :", "Oui", true);
+            embedPerm.addField("Peut rendre muet :", "Oui", true);
         else
-            embedOK.addField("Peut rendre muet : ", "Non", true);
+            embedPerm.addField("Peut rendre muet : ", "Non", true);
 
+        const collector = interaction.channel.createMessageComponentCollector();
 
-        return interaction.reply({embeds: [embedOK]});
+        collector.on('collect', async i => {
+            if (i.customId === 'primary') {
+                await i.update({ embeds: [embedPerm], components: [retour] });
+            } else if (i.customId === 'perm'){
+                await i.update({ embeds: [embedBase], components: [perm] })
+            }
+        });
+
+        return interaction.reply({embeds: [embedBase], components: [perm]});
     },
 };
