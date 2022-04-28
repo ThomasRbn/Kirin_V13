d67ae01a3d91d1c9398e8046736148f2c8f046e1
Thomas Robineau <robineau1u@etu.univ-lorraine.fr>
2022-04-28 22:57:28 +0200

    Ajout verif du ban

--- commands/unban.js
+++ commands/unban.js
@@ -9,37 +9,55 @@ module.exports = {
         .addBooleanOption(option => option.setName("prevenir").setDescription("Prevenir du débannissement").setRequired(true)),
 
     async execute(interaction) {
-        const idUser = interaction.options.getString('id');
+        try {
+            const idUser = interaction.options.getString('id');
+            const utili = await interaction.client.users.fetch(idUser);
 
-        let prevenir = interaction.options.getBoolean('prevenir');
-        if (prevenir){
-            prevenir = 'Oui';
-        } else{
-            prevenir = 'Non';
+            let prevenir = interaction.options.getBoolean('prevenir');
+            if (prevenir){
+                prevenir = 'Oui';
+            } else{
+                prevenir = 'Non';
+            }
+
+            await interaction.guild.bans.fetch(idUser);
+
+            const fetchedLogs = await interaction.guild.fetchAuditLogs({
+                limit: 1,
+                type: 'MEMBER_BAN_ADD',
+            });
+
+            const banLog = fetchedLogs.entries.first();
+            console.log(banLog);
+            const banReason = banLog.reason;
+            const banDate = banLog.createdAt.toLocaleTimeString('fr-FR');
+
+            console.log(banReason, banDate);
+
+            const embedUser = new MessageEmbed()
+                .setColor("ORANGE")
+                .setTitle('Bannissement de ' + await interaction.client.users.fetch(idUser))
+                .addField("Banni le :", banDate, true)
+                .addField("Raison :", banReason, true);
+
+            const embedUnban = new MessageEmbed()
+                .setColor("GREEN")
+                .setTitle(await interaction.client.users.fetch(idUser) + " à bien été débanni");
+
+            await interaction.guild.members.unban(idUser);
+            await interaction.reply({embeds: [embedUnban]});
+
+        } catch(e) { // Si pas de ban
+            if(e.code !== 10026) throw e;
+            const idUser = interaction.options.getString('id');
+            const utili = await interaction.client.users.fetch(idUser);
+
+            console.log(utili);
+            const embedNull = new MessageEmbed()
+                .setTitle(utili.tag + " n'est pas banni de ce serveur")
+                .setColor("RED");
+
+            return interaction.reply({embeds: [embedNull]});
         }
-
-        const fetchedLogs = await interaction.guild.fetchAuditLogs({
-            limit: 1,
-            type: 'MEMBER_BAN_ADD',
-        });
-
-        const banLog = fetchedLogs.entries.first();
-        const banReason = banLog.reason;
-        const banDate = banLog.createdAt.toLocaleTimeString('fr-FR');
-
-        const embedUser = new MessageEmbed()
-            .setColor("ORANGE")
-            .setTitle('Bannissement de ' + await interaction.client.users.fetch(idUser))
-            .addField("Banni le :", banDate, true)
-            .addField("Raison :", banReason, true);
-
-        const embedUnban = new MessageEmbed()
-            .setColor("GREEN")
-            .setTitle(await interaction.client.users.fetch(idUser) + "à bien été débanni");
-
-        await interaction.guild.members.unban(idUser);
-        await interaction.reply({embeds: [embedUnban]});
-
-
     },
 };
